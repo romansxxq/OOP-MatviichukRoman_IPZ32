f82c9134857ff37d5f2e2c4acbe559739b4d5462
romatvX_ <146443792+romansxxq@users.noreply.github.com>
2025-09-03 13:44:24 +0300

    MyFirstOOP

---
+++ Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Figure
+{
+
+    public double Area { get; private set; }
+
+    public Figure(double area)
+    {
+        Area = area;
+    }
+    ~Figure()
+    {
+        Console.WriteLine("Destructor called");
+    }
+    public void GetFigure()
+    {
+        Console.WriteLine($"Area: {Area}");
+    }
+}
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        Figure figure = new Figure(25.5);
+        figure.GetFigure();
+    }
+}
