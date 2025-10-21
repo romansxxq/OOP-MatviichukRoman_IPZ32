28d4f5630bfed369c6b55686f171411066158b04
romatvX_ <asssfsa010@gmail.com>
2025-10-22 02:50:40 +0300

    add lab4v8

--- Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b14d5cb89668ff92e2d68e3d0293881662422fc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7308aab4bd6fd06da65eaa23dcb55b6118effb97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
