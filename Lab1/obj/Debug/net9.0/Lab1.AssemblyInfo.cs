2b0c6b183c922851c0b61a0e9df91d8fcaeda44d
romatvX_ <146443792+romansxxq@users.noreply.github.com>
2025-11-12 20:32:17 +0200

    update readme

--- Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a0a9fed7b494a9ccb96fe45b22cf2c7d7801433")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e91976974981582f5f1908b06767a0f918e890b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
