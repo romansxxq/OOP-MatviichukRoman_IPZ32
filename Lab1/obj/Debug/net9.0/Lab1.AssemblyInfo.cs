ef85e3747220a1cca0a9702dc5225ec684eb8528
romatvX_ <146443792+romansxxq@users.noreply.github.com>
2025-11-26 23:52:22 +0200

    test

--- Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e91976974981582f5f1908b06767a0f918e890b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4122f7480eaa2672d22060598cb94478904c213b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab6v8/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Func<int, int, bool> isPrimeNumCheck = (num, divisor) =>
+        {
+            if (divisor <= 1) return true;
+            if (num % divisor == 0) return false;
+            return isPrimeNumCheck(num, divisor - 1);
+        };
+        Console.WriteLine("Hello, World!");
+    }
+}
