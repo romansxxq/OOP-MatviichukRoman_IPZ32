0bf405b361f88f65e8ce93fbe5fe5b1f94a6d948
romatvX_ <asssfsa010@gmail.com>
2026-01-26 00:17:29 +0200

    Lab №21: OCP and patterns (Stategy/Factory)

---
+++ Lab21/Task1/Program.cs
@@ -0,0 +1,88 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Type of shipping (Standard, Express, International, Night): ");
+        string? input = Console.ReadLine() ?? "";
+        // Validate input
+        var isValid = Enum.GetValues(typeof(ShippingType))
+            .Cast<ShippingType>()
+            .Any(t => t.ToString().Equals(input, StringComparison.OrdinalIgnoreCase));
+
+        if (!isValid)
+        {
+            Console.WriteLine("Invalid shipping type.");
+            return;
+        }
+        // Convert input to ShippingType enum
+        ShippingType shippingType = (ShippingType)Enum.Parse(typeof(ShippingType), input, true);
+
+        Console.WriteLine("Distance (in km): ");
+        string? distanceInput = Console.ReadLine() ?? "0";
+        decimal distance = decimal.Parse(distanceInput);
+
+        Console.WriteLine("Weight (in kg): ");
+        string ? weightInput = Console.ReadLine() ?? "0";
+        decimal weight = decimal.Parse(weightInput);
+
+        IShippingStrategy strategy = ShippingStretegyFactory.CreateShippingStrategy(shippingType);
+
+        DeliveryService service = new DeliveryService();
+        decimal cost = service.CalculateDeliverycost(distance, weight, strategy);
+        Console.WriteLine($"The delivery cost is: {cost} USD.");
+    }
+}
+public enum ShippingType
+{
+    Standard,
+    Express,
+    International,
+    Night
+}
+public interface IShippingStrategy
+{
+    decimal CalculateCost(decimal distance, decimal weight);
+}
+public class StandardShippingStrategy : IShippingStrategy
+{
+    public decimal CalculateCost(decimal distance, decimal weight) 
+        => distance * 2.5m + weight * 0.5m;
+}
+public class ExpressShippingStrategy : IShippingStrategy
+{
+    public decimal CalculateCost(decimal distance, decimal weight)
+        => distance * 2.5m + weight * 1.0m + 50.0m;
+}
+public class InternationalShippingStrategy : IShippingStrategy
+{
+    public decimal CalculateCost(decimal distance, decimal weight)
+        => distance * 5.0m + weight * 2.0m + 0.15m;
+}
+public class NightShippingStrategy : IShippingStrategy
+{
+    public decimal CalculateCost(decimal distance, decimal weight)
+        => distance * 3.0m + weight * 1.5m + 100.0m;
+}
+public class ShippingStretegyFactory
+{
+    public static IShippingStrategy CreateShippingStrategy(ShippingType shippingType)
+    {
+        return shippingType switch
+        {
+            ShippingType.Standard => new StandardShippingStrategy(),
+            ShippingType.Express => new ExpressShippingStrategy(),
+            ShippingType.International => new InternationalShippingStrategy(),
+            _ => throw new ArgumentOutOfRangeException(nameof(shippingType), "Invalid shipping type")
+        };
+    }
+}
+public class DeliveryService
+{
+    public decimal CalculateDeliverycost(
+    decimal distance,
+    decimal weight,
+    IShippingStrategy strategy)
+    {
+        return strategy.CalculateCost(distance, weight);
+    }
+}
---
+++ Lab21/Task2v8/Program.cs
@@ -0,0 +1,118 @@
+﻿
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Type of insurance (Basic, Extended, FullCoverage, PremiumCoverage): ");
+        string? input = Console.ReadLine() ?? "";
+
+        var isValid = Enum.GetValues(typeof(InsuranceType))
+            .Cast<InsuranceType>()
+            .Any(t => t.ToString().Equals(input, StringComparison.OrdinalIgnoreCase));
+        
+        if (!isValid)
+        {
+            Console.WriteLine("Invalid insurance type.");
+            return;
+        }
+
+        InsuranceType insuranceType = (InsuranceType)Enum.Parse(typeof(InsuranceType), input, true);
+
+        Console.WriteLine("Driver Age: ");
+        string? ageInput = Console.ReadLine() ?? "0";
+
+        System.Console.WriteLine("Driver Experience (in years): ");
+        string? experienceInput = Console.ReadLine() ?? "0";
+
+        Console.WriteLine("Price of the car (in USD): ");
+        string? priceCarInput = Console.ReadLine() ?? "0";
+
+        IInsuranceStrategy strategy = InsuranceStrategyFactory.CreateInsuranceStrategy(insuranceType);
+        InsuranceCalculator calculator = new InsuranceCalculator();
+
+        decimal insuranceCost = calculator.CalculateInsurance(
+            int.Parse(ageInput),
+            int.Parse(experienceInput),
+            decimal.Parse(priceCarInput),
+            strategy);
+        
+        Console.WriteLine($"The insurance cost is: {insuranceCost} USD.");
+    }
+}
+public enum InsuranceType
+{
+    Basic,
+    Extended,
+    FullCoverage,
+    PremiumCoverage
+}
+public interface IInsuranceStrategy
+{
+    decimal CalculateInsurance(int driverAge, int driverExperience, decimal priceAuto);
+}
+
+public class BasicInsuranceStrategy : IInsuranceStrategy
+{
+    public decimal CalculateInsurance(int driverAge, int driverExperience, decimal priceCar)
+    {
+        decimal ageFactor = driverAge < 22 ? 1.5m : 1.0m;
+        decimal experienceFactor = driverExperience < 2 ? 1.2m : 1.0m;
+        decimal baseRate = 0.02m;
+        return priceCar * baseRate * ageFactor * experienceFactor;
+    }
+}
+public class ExtendedInsuranceStrategy : IInsuranceStrategy
+{
+    public decimal CalculateInsurance(int driverAge, int driverExperience, decimal priceCar)
+    {
+        decimal ageFactor = driverAge < 25 ? 1.0m : 1.0m;
+        decimal experienceFactor = driverExperience < 2 ? 1.3m : 1.0m;
+        decimal baseRate = 0.03m;
+        return priceCar * baseRate * ageFactor * experienceFactor;
+    }
+}
+public class FullCoverageStrategy : IInsuranceStrategy
+{
+    public decimal CalculateInsurance(int driverAge, int driverExperience, decimal priceCar)
+    {
+        decimal ageFactor = driverAge < 30 ? 1.7m : 1.0m;
+        decimal experienceFactor = driverExperience < 5 ? 1.5m : 1.0m;
+        decimal baseRate = 0.05m;
+        return priceCar * baseRate * ageFactor * experienceFactor;
+    }
+}
+public class PremiumCoverageStrategy : IInsuranceStrategy
+{
+    public decimal CalculateInsurance(int driverAge, int driverExperience, decimal priceCar)
+    {
+        decimal ageFactor = driverAge < 35 ? 2.0m : 1.0m;
+        decimal experienceFactor = driverExperience < 10 ? 1.7m : 1.0m;
+        decimal baseRate = 0.07m;
+        return priceCar * baseRate * ageFactor * experienceFactor;
+    }
+}
+
+public static class InsuranceStrategyFactory
+{
+    public static IInsuranceStrategy CreateInsuranceStrategy(InsuranceType insuranceType)
+    {
+        return insuranceType switch
+        {
+            InsuranceType.Basic => new BasicInsuranceStrategy(),
+            InsuranceType.Extended => new ExtendedInsuranceStrategy(),
+            InsuranceType.FullCoverage => new FullCoverageStrategy(),
+            InsuranceType.PremiumCoverage => new PremiumCoverageStrategy(),
+            _ => throw new ArgumentException("Invalid insurance type")
+        };
+    }
+}
+
+public class InsuranceCalculator
+{
+    public decimal CalculateInsurance(
+        int driverAge,
+        int driverExperience,
+        decimal priceCar,
+        IInsuranceStrategy strategy)
+        => strategy.CalculateInsurance(driverAge, driverExperience, priceCar);
+}
