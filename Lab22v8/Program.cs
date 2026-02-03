10cf383f01e0957efa81f1ada0d98f60b1961302
romatvX_ <asssfsa010@gmail.com>
2026-02-04 01:42:44 +0200

    Lab22: LCP

---
+++ Lab22v8/Program.cs
@@ -0,0 +1,106 @@
+﻿// Bad LSP
+// class CustomList
+// {
+//     protected List<int> items = new List<int>();
+//     public virtual void Add(int item)
+//     {
+//         items.Add(item);
+//     }
+//     public void Print()
+//     {
+//         foreach (var item in items)
+//         {
+//             Console.Write(item + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// class ReadOnlyList : CustomList
+// {
+//     public override void Add(int item)
+//     {
+//         throw new InvalidOperationException("Cannot add items to a read-only list.");
+//     }
+// }
+// class Program
+// {
+//     static void AddItems(CustomList list)
+//     {
+//         list.Add(10);
+
+//     }
+//     static void Main(string[] args)
+//     {
+//        Console.WriteLine("LSP Violation Example:");
+//        CustomList normalist = new CustomList();
+//         AddItems(normalist);
+//         normalist.Print();
+//         CustomList readOnlyList = new ReadOnlyList();
+//         try
+//         {
+//             AddItems(readOnlyList);
+//         }
+//         catch (InvalidOperationException ex)
+//         {
+//             Console.WriteLine("Exception: " + ex.Message);
+//         }
+//     }
+// }
+
+// Good LSP
+class Program
+{
+    static void AddItems(IMutiableList list)
+    {
+        list.Add(10);
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("LSP Compliance Example:");
+        IMutiableList normalist = new CustomList();
+        AddItems(normalist);
+        normalist.Print();
+        IReadOnlyList readOnlyList = new ReadOnlyList(new List<int> { 1, 2, 3 });
+        readOnlyList.Print();
+    }
+}
+interface IReadOnlyList
+{
+    void Print();
+}
+interface IMutiableList : IReadOnlyList
+{
+    void Add(int item);
+}
+class CustomList : IMutiableList
+{
+    protected List<int> items = new List<int>();
+    public void Add(int item)
+    {
+        items.Add(item);
+    }
+    public void Print()
+    {
+        foreach (var item in items)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
+class ReadOnlyList : IReadOnlyList
+{
+    protected List<int> items = new List<int>();
+    public ReadOnlyList(List<int> initialItems)
+    {
+        items.AddRange(initialItems);
+    }
+    public void Print()
+    {
+        foreach (var item in items)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
