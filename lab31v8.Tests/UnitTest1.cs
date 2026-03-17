f850ec72400e06573fafcfa30d59f4b169d834c8
romatvX_ <146443792+romansxxq@users.noreply.github.com>
2026-03-18 00:40:55 +0200

    add moq tests and lab31v8

---
+++ lab31v8.Tests/UnitTest1.cs
@@ -0,0 +1,115 @@
+﻿using Moq;
+using lab31v8;
+using Xunit;
+
+namespace lab31v8.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CreateReport_CallsGetDataAndGeneratePdf()
+    {
+        var mockDataSource = new Mock<IDataSource>();
+        var mockPdfGenerator = new Mock<IPdfGenerator>();
+        mockDataSource.Setup(ds => ds.GetData()).Returns("Test data");
+
+        var service = new ReportService(mockDataSource.Object, mockPdfGenerator.Object);
+        service.CreateReport();
+
+        mockDataSource.Verify(ds => ds.GetData(), Times.Once);
+        mockPdfGenerator.Verify(pg => pg.GeneratePdf("Test data"), Times.Once);
+    }
+
+    [Fact]
+    public void CreateReport_GeneratePdfCalledWithCorrectData()
+    {
+        var mockDataSource = new Mock<IDataSource>();
+        var mockPdfGenerator = new Mock<IPdfGenerator>();
+        mockDataSource.Setup(ds => ds.GetData()).Returns("Special data");
+
+        var service = new ReportService(mockDataSource.Object, mockPdfGenerator.Object);
+        service.CreateReport();
+
+        mockPdfGenerator.Verify(pg => pg.GeneratePdf("Special data"), Times.Once);
+    }
+
+    [Fact]
+    public void CreateReport_DoesNotCallGeneratePdfIfNoData()
+    {
+        var mockDataSource = new Mock<IDataSource>();
+        var mockPdfGenerator = new Mock<IPdfGenerator>();
+        mockDataSource.Setup(ds => ds.GetData()).Returns(string.Empty);
+
+        var service = new ReportService(mockDataSource.Object, mockPdfGenerator.Object);
+        service.CreateReport();
+
+        mockPdfGenerator.Verify(pg => pg.GeneratePdf(string.Empty), Times.Once);
+    }
+
+    [Fact]
+    public void CreateReport_CallsGeneratePdfExactlyOnce()
+    {
+        var mockDataSource = new Mock<IDataSource>();
+        var mockPdfGenerator = new Mock<IPdfGenerator>();
+        mockDataSource.Setup(ds => ds.GetData()).Returns("Data");
+
+        var service = new ReportService(mockDataSource.Object, mockPdfGenerator.Object);
+        service.CreateReport();
+
+        mockPdfGenerator.Verify(pg => pg.GeneratePdf(It.IsAny<string>()), Times.Once);
+    }
+
+    [Fact]
+    public void CreateReport_GetDataReturnsNull()
+    {
+        var mockDataSource = new Mock<IDataSource>();
+        var mockPdfGenerator = new Mock<IPdfGenerator>();
+        mockDataSource.Setup(ds => ds.GetData()).Returns((string)null);
+
+        var service = new ReportService(mockDataSource.Object, mockPdfGenerator.Object);
+        service.CreateReport();
+
+        mockPdfGenerator.Verify(pg => pg.GeneratePdf(null), Times.Once);
+    }
+
+    [Fact]
+    public void CreateReport_VerifyNoOtherCalls()
+    {
+        var mockDataSource = new Mock<IDataSource>();
+        var mockPdfGenerator = new Mock<IPdfGenerator>();
+        mockDataSource.Setup(ds => ds.GetData()).Returns("Data");
+
+        var service = new ReportService(mockDataSource.Object, mockPdfGenerator.Object);
+        service.CreateReport();
+
+        mockDataSource.Verify(ds => ds.GetData(), Times.Once);
+        mockPdfGenerator.Verify(pg => pg.GeneratePdf("Data"), Times.Once);
+        mockPdfGenerator.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public void CreateReport_MultipleReportsCallsGeneratePdfMultipleTimes()
+    {
+        var mockDataSource = new Mock<IDataSource>();
+        var mockPdfGenerator = new Mock<IPdfGenerator>();
+        mockDataSource.Setup(ds => ds.GetData()).Returns("Multi");
+
+        var service = new ReportService(mockDataSource.Object, mockPdfGenerator.Object);
+        service.CreateReport();
+        service.CreateReport();
+
+        mockPdfGenerator.Verify(pg => pg.GeneratePdf("Multi"), Times.Exactly(2));
+    }
+
+    [Fact]
+    public void CreateReport_SetupThrowsException()
+    {
+        var mockDataSource = new Mock<IDataSource>();
+        var mockPdfGenerator = new Mock<IPdfGenerator>();
+        mockDataSource.Setup(ds => ds.GetData()).Throws(new Exception("Data error"));
+
+        var service = new ReportService(mockDataSource.Object, mockPdfGenerator.Object);
+        Assert.Throws<Exception>(() => service.CreateReport());
+        mockPdfGenerator.Verify(pg => pg.GeneratePdf(It.IsAny<string>()), Times.Never);
+    }
+}
---
+++ lab31v8/Program.cs
@@ -0,0 +1,49 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        IDataSource dataSource = new DataSource();
+        IPdfGenerator pdfGenerator = new PdfGenerator();
+        ReportService reportService = new ReportService(dataSource, pdfGenerator);
+        reportService.CreateReport();
+    }
+}
+public interface IDataSource
+{
+    string GetData();
+}
+public interface IPdfGenerator
+{
+    void GeneratePdf(string data);
+}
+public class DataSource : IDataSource
+{
+    public string GetData()
+    {
+        return "Sample data from DataSource.";
+    }
+}
+public class PdfGenerator : IPdfGenerator
+{
+    public void GeneratePdf(string data)
+    {
+        Console.WriteLine($"Generating PDF with data: {data}");
+    }
+}
+public class ReportService
+{
+    private readonly IDataSource _dataSource;
+    private readonly IPdfGenerator _pdfGenerator;
+
+    public ReportService(IDataSource dataSource, IPdfGenerator pdfGenerator)
+    {
+        _dataSource = dataSource;
+        _pdfGenerator = pdfGenerator;
+    }
+
+    public void CreateReport()
+    {
+        string data = _dataSource.GetData();
+        _pdfGenerator.GeneratePdf(data);
+    }
+}
