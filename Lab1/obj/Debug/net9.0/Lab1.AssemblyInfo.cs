aad4521e124b90710436fd05f286419f60636ed0
romatvX_ <146443792+romansxxq@users.noreply.github.com>
2026-04-22 00:04:00 +0300

    .

--- Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f4a5eb0aa4886b2003d50ee424fd6084b53431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+342ad27f0f906e59d8f2e95baff798a23a75f53b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
